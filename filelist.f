2314dfc38bb94a24f477fb694b9ed1cfc87a187e
retar-kamuy <hoshi.takumi.jp@gmail.com>
2022-12-11 12:44:13 +0900

    Change veriloghdl用環境を追加

---
+++ filelist.f
@@ -0,0 +1,11 @@
+fmrv32im_core.sv
+modules/axilm_v1/src/fmrv32im_axilm.v
+modules/axim_v1/src/fmrv32im_axim.v
+modules/cache_v1/src/fmrv32im_cache.v
+modules/dbussel_v1/src/fmrv32im_dbussel.v
+modules/plic_v1/src/fmrv32im_plic.v
+modules/timer_v1/src/fmrv32im_timer.v
+
+src/tb_fmrv32im_core.v
+src/tb_axil_slave_model.v
+src/tb_axi_slave_model.v
