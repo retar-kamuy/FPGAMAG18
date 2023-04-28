fc97731d0c37700ec9d7d33f12df3864571e328e
Hoshi Takumi <hoshi.takumi.jp@gmail.com>
2023-04-28 23:04:55 +0900

    Failing rv32ui-p-fence_i and rv32ui-p-remu

--- src/tb_fmrv32im_core.sv
+++ src/tb_fmrv32im_core.sv
@@ -2,8 +2,6 @@
 `include "vunit_defines.svh"
 
 module tb_fmrv32im_core;
-   localparam MEM_FILE = "imem.hex";
-
    reg RST_N;
    reg CLK;
 
@@ -96,65 +94,109 @@ module tb_fmrv32im_core;
    wire        MM_AXI_RVALID;
    wire        MM_AXI_RREADY;
 
+   logic [31:0] rslt;
+
    // Clock
    localparam CLK100M = 10;
    always begin
       #(CLK100M/2) CLK <= ~CLK;
    end
 
-   reg [31:0] rslt;
-   always @(posedge CLK) begin
-      if((u_fmrv32im_core.dbus_addr == 32'h0000_0800) &
-	 (u_fmrv32im_core.dbus_wstb == 4'hF))
-	begin
-	   rslt <= u_fmrv32im_core.dbus_wdata;
-	end
-   end
-   
+   task load_elf(input string path);
+      $info("Load ELF: Simulatin Start %s\n", path);
+      $readmemh(path, u_fmrv32im_core.u_fmrv32im_cache.imem, 0, 1023);
+      $readmemh(path, u_fmrv32im_core.u_fmrv32im_cache.dmem, 0, 1023);
+
+      @(posedge CLK);
+      $info("Process Start");
+   endtask
+
    // Sinario
    `TEST_SUITE begin
       `TEST_SUITE_SETUP begin
+         $info("Running test suite setup code");
          RST_N = 1'b0;
          CLK   = 1'b0;
 
          INTERRUPT = 0;
+      end
 
+      `TEST_CASE_SETUP begin
+         $info("Running test case setup code");
          @(posedge CLK);
          RST_N = 1'b1;
-         $display("============================================================");
-         $display("Simulatin Start");
-         $display("============================================================");
+         $info("Simulation Start");
       end
-      `TEST_CASE("rv32ui-p-add") begin
-         localparam MEM_FILE = "rv32ui-p-add.hex";
-         $readmemh(MEM_FILE, u_fmrv32im_core.u_fmrv32im_cache.imem, 0, 1023);
-         $readmemh(MEM_FILE, u_fmrv32im_core.u_fmrv32im_cache.dmem, 0, 1023);
-         @(posedge CLK);
 
-         $display("============================================================");
-         $display("Process Start");
-         $display("============================================================");
+      `TEST_CASE("rv32ui-p-add") load_elf("rv32ui-p-add.hex");
+      `TEST_CASE("rv32ui-p-addi") load_elf("rv32ui-p-addi.hex");
+      `TEST_CASE("rv32ui-p-and") load_elf("rv32ui-p-and.hex");
+      `TEST_CASE("rv32ui-p-andi") load_elf("rv32ui-p-andi.hex");
+      `TEST_CASE("rv32ui-p-auipc") load_elf("rv32ui-p-auipc.hex");
+      `TEST_CASE("rv32ui-p-beq") load_elf("rv32ui-p-beq.hex");
+      `TEST_CASE("rv32ui-p-bge") load_elf("rv32ui-p-bge.hex");
+      `TEST_CASE("rv32ui-p-bgeu") load_elf("rv32ui-p-bgeu.hex");
+      `TEST_CASE("rv32ui-p-blt") load_elf("rv32ui-p-blt.hex");
+      `TEST_CASE("rv32ui-p-bltu") load_elf("rv32ui-p-bltu.hex");
+      `TEST_CASE("rv32ui-p-bne") load_elf("rv32ui-p-bne.hex");
+      `TEST_CASE("rv32ui-p-fence_i") load_elf("rv32ui-p-fence_i.hex");
+      `TEST_CASE("rv32ui-p-jal") load_elf("rv32ui-p-jal.hex");
+      `TEST_CASE("rv32ui-p-jalr") load_elf("rv32ui-p-jalr.hex");
+      `TEST_CASE("rv32ui-p-lb") load_elf("rv32ui-p-lb.hex");
+      `TEST_CASE("rv32ui-p-lbu") load_elf("rv32ui-p-lbu.hex");
+      `TEST_CASE("rv32ui-p-lh") load_elf("rv32ui-p-lh.hex");
+      `TEST_CASE("rv32ui-p-lhu") load_elf("rv32ui-p-lhu.hex");
+      `TEST_CASE("rv32ui-p-lui") load_elf("rv32ui-p-lui.hex");
+      `TEST_CASE("rv32ui-p-lw") load_elf("rv32ui-p-lw.hex");
+      `TEST_CASE("rv32ui-p-or") load_elf("rv32ui-p-or.hex");
+      `TEST_CASE("rv32ui-p-ori") load_elf("rv32ui-p-ori.hex");
+      `TEST_CASE("rv32ui-p-sb") load_elf("rv32ui-p-sb.hex");
+      `TEST_CASE("rv32ui-p-sh") load_elf("rv32ui-p-sh.hex");
+      `TEST_CASE("rv32ui-p-simple") load_elf("rv32ui-p-simple.hex");
+      `TEST_CASE("rv32ui-p-sll") load_elf("rv32ui-p-sll.hex");
+      `TEST_CASE("rv32ui-p-slli") load_elf("rv32ui-p-slli.hex");
+      `TEST_CASE("rv32ui-p-slt") load_elf("rv32ui-p-slt.hex");
+      `TEST_CASE("rv32ui-p-slti") load_elf("rv32ui-p-slti.hex");
+      `TEST_CASE("rv32ui-p-sltiu") load_elf("rv32ui-p-sltiu.hex");
+      `TEST_CASE("rv32ui-p-sltu") load_elf("rv32ui-p-sltu.hex");
+      `TEST_CASE("rv32ui-p-sra") load_elf("rv32ui-p-sra.hex");
+      `TEST_CASE("rv32ui-p-srai") load_elf("rv32ui-p-srai.hex");
+      `TEST_CASE("rv32ui-p-srl") load_elf("rv32ui-p-srl.hex");
+      `TEST_CASE("rv32ui-p-srli") load_elf("rv32ui-p-srli.hex");
+      `TEST_CASE("rv32ui-p-sub") load_elf("rv32ui-p-sub.hex");
+      `TEST_CASE("rv32ui-p-sw") load_elf("rv32ui-p-sw.hex");
+      `TEST_CASE("rv32ui-p-xor") load_elf("rv32ui-p-xor.hex");
+      `TEST_CASE("rv32ui-p-xori") load_elf("rv32ui-p-xori.hex");
+      `TEST_CASE("rv32um-p-div") load_elf("rv32um-p-div.hex");
+      `TEST_CASE("rv32um-p-divu") load_elf("rv32um-p-divu.hex");
+      `TEST_CASE("rv32um-p-mul") load_elf("rv32um-p-mul.hex");
+      `TEST_CASE("rv32um-p-mulh") load_elf("rv32um-p-mulh.hex");
+      `TEST_CASE("rv32um-p-mulhsu") load_elf("rv32um-p-mulhsu.hex");
+      `TEST_CASE("rv32um-p-mulhu") load_elf("rv32um-p-mulhu.hex");
+      `TEST_CASE("rv32um-p-rem") load_elf("rv32um-p-rem.hex");
+      `TEST_CASE("rv32um-p-remu") load_elf("rv32um-p-remu.hex");
 
-         wait((u_fmrv32im_core.dbus_addr == 32'h0000_0800) & 
-	      (u_fmrv32im_core.dbus_wstb == 4'hF));
-	
+      `TEST_CASE_CLEANUP begin
+         wait(
+            (u_fmrv32im_core.dbus_addr == 32'h0000_0800) &
+	         (u_fmrv32im_core.dbus_wstb == 4'hF)
+         );
+         rslt <= u_fmrv32im_core.dbus_wdata;
          repeat(10) @(posedge CLK);
       end
 
       `TEST_SUITE_CLEANUP begin
-         $display("============================================================");
-         $display("Simulatin Finish");
-         $display("============================================================");
-         $display("Result: %8x\n", rslt);
+         $info("Simulatin Finish");
+         assert(rslt === 32'd1) begin
+            $info("Success Result: %8x\n", rslt);
+         end
+            else begin
+               $fatal("Error Result: %8x\n", rslt);
+            end
       end
    end
 
-   fmrv32im_core 
-    #(
-      .MEM_FILE ("imem.hex")
-    )
-    u_fmrv32im_core
-     (
+   fmrv32im_core u_fmrv32im_core (
       .RST_N          (RST_N),
       .CLK            (CLK),
 
