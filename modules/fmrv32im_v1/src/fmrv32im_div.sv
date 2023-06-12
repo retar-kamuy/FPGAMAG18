353bebda8e49f0665f566ac7f1f99d807b4c43c9
Hoshi Takumi <hoshi.takumi.jp@gmail.com>
2023-06-12 23:03:51 +0900

    rename

--- modules/fmrv32im_v1/src/fmrv32im_div.sv
+++ modules/fmrv32im_v1/src/fmrv32im_div.sv
@@ -1,39 +1,34 @@
 module fmrv32im_div (
-    input 	      RST_N,
-    input 	      CLK,
-
-    input 	      INST_DIV,
-    input 	      INST_DIVU,
-    input 	      INST_REM,
-    input 	      INST_REMU,
-
-    input       [31:0]        RS1,
-    input       [31:0]        RS2,
-
-    output wire            WAIT,
-    output wire         READY,
-    output reg  [31:0]  RD
+    input               RST_N,
+    input               CLK,
+    input               INST_DIV,
+    input               INST_DIVU,
+    input               INST_REM,
+    input               INST_REMU,
+    input        [31:0] RS1,
+    input        [31:0] RS2,
+    output logic        WAIT,
+    output logic        READY,
+    output logic [31:0] RD
 );
 
-    reg [1:0]      state;
+    enum logic [1:0] {
+        S_IDLE  = 0,
+        S_EXEC  = 1,
+        S_FIN   = 2
+    } state;
 
-    localparam S_IDLE = 2'd0;
-    localparam S_EXEC = 2'd1;
-    localparam S_FIN = 2'd2;
-
-    wire             start;
-
+    logic start;
     assign start = INST_DIV | INST_DIVU | INST_REM | INST_REMU;
 
-    reg [31:0]     dividend;
-    reg [62:0]     divisor;
-    reg [31:0]     quotient, quotient_mask;
-    reg              outsign;
-    reg              reg_inst_div, reg_inst_rem;
+    logic [31:0] dividend;
+    logic [62:0] divisor;
+    logic [31:0] quotient, quotient_mask;
+    logic outsign;
+    logic reg_inst_div, reg_inst_rem;
 
-    always @(posedge CLK)
+    always @(posedge CLK or negedge RST_N)
         if (~RST_N) begin
-            state <= S_IDLE;
             dividend <= 0;
             divisor <= 0;
             outsign <= 0;
@@ -42,11 +37,10 @@ module fmrv32im_div (
             reg_inst_div <= 0;
             reg_inst_rem <= 0;
             RD <= 0;
-        end else begin
+        end else
             case (state)
                 S_IDLE:
                     if (start) begin
-                        state <= S_EXEC;
                         dividend <= ((INST_DIV | INST_REM) & RS1[31]) ? -RS1 : RS1;
                         divisor[62:31] <= ((INST_DIV | INST_REM) & RS2[31]) ? -RS2 : RS2;
                         divisor[30:0] <= 31'd0;
@@ -57,9 +51,6 @@ module fmrv32im_div (
                         reg_inst_rem <= INST_REM | INST_REMU;
                     end
                 S_EXEC: begin
-                    if (!quotient_mask) begin
-                        state <= S_FIN;
-                    end
                     if (divisor <= dividend) begin
                         dividend <= dividend - divisor;
                         quotient <= quotient | quotient_mask;
@@ -67,17 +58,29 @@ module fmrv32im_div (
                     divisor <= divisor >> 1;
                     quotient_mask <= quotient_mask >> 1;
                 end
-                S_FIN: begin
-                    state <= S_IDLE;
+                S_FIN:
                     if (reg_inst_div) 
                         RD <= outsign ? -quotient : quotient;
                     else if (reg_inst_rem)
                         RD <= outsign ? -dividend : dividend;
                     else
                         RD <= 32'd0;
-                end
             endcase
-        end
+
+    always @(posedge CLK or negedge RST_N)
+        if (~RST_N)
+            state <= S_IDLE;
+        else
+            case (state)
+                S_IDLE:
+                    if (start)
+                        state <= S_EXEC;
+                S_EXEC:
+                    if (!quotient_mask)
+                        state <= S_FIN;
+                S_FIN:
+                    state <= S_IDLE;
+            endcase
 
     assign WAIT  = (state != S_IDLE);
     assign READY = (state == S_FIN);
